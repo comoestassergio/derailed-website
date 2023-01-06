9d60e1fcc2d4a16e6d8017e149fb73c79b3fb42f
comoestassergio <dirtycrew666@gmail.com>
2023-01-06 21:40:53 +0300

    Add hero section for mobile

---
+++ components/HeroMobile.jsx
@@ -0,0 +1,16 @@
+
+
+export default function HeroMobile () {
+    return (
+        <div className='bg-primary w-full h-screen'>
+            <div className="flex flex-col pl-3 pt-3">
+                <h1 className='text-[13rem] leading-[100%] text-secondary  uppercase'>de</h1>
+                <h1 className='text-[13rem] leading-[100%] text-secondary uppercase'>ra</h1>
+                <h1 className='text-[13rem] leading-[100%] text-secondary uppercase'>il</h1>
+                <h1 className='text-[13rem] leading-[100%] text-secondary uppercase'>ed</h1>
+                
+            </div>
+            <p className="text-white ml-6 mt-6 text-lg opacity-80">shape reality around you</p>
+        </div>
+    )
+}
